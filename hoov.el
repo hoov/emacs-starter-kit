2eadcf13708eb985c362fbcbec89c8315fdab13a
Tom Hoover <thoover@alum.mit.edu>
2010-04-20 20:29:05 -0700

    Lots of changes. Color themes, new ropemode/ropemacs, etc

--- hoov.el
+++ hoov.el
@@ -4,6 +4,10 @@
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
+(load (concat dotfiles-dir "hoov/color-theme-hoov.el"))
+(load (concat dotfiles-dir "vendor/color-theme-irblack.el"))
+(load (concat dotfiles-dir "vendor/color-theme-tango.el"))
+(color-theme-hoov)
 
 ;; Pymacs
 (require 'pymacs)
@@ -11,6 +15,9 @@
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
 (setq ac-ropemacs-loaded t)
+
+(add-hook 'python-mode-hook '(lambda ()
+                               (local-set-key (kbd "RET") 'newline-and-indent)))
 
 ;; yasnippet
 
@@ -22,7 +29,7 @@
 ;; Auto Complete
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/auto-complete"))
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories (concat dotfiles-dir "ac-dict"))
+(add-to-list 'ac-dictionary-directories (concat dotfiles-dir "/vendor/auto-complete/dict"))
 (ac-config-default)
 
 (defun ac-python-mode-setup ()
@@ -30,5 +37,21 @@
   (setq ac-sources (append '(ac-source-yasnippet) ac-sources)))
 (add-hook 'python-mode-hook 'ac-python-mode-setup)
 
-; 
-; (setq ac-sources (append '(ac-source-yasnippet) ac-sources)))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/textmate.el"))
+(require 'textmate)
+(textmate-mode)
+(require 'peepopen)
+
+;; Mercurial support
+(require 'mercurial)
+
+
+
+;; Pycomplexity
+;(add-to-list 'load-path (concat dotfiles-dir "/vendor/pycomplexity"))
+;(require 'linum)
+;(require 'pycomplexity)
+;(add-hook 'python-mode-hook
+;    (function (lambda ()
+;        (pycomplexity-mode)
+;        (linum-mode))))
