688409e497dec58aa8c398f91a4d0cb21dd568f3
Tom Hoover <thoover@alum.mit.edu>
2010-05-25 14:25:17 -0700

    Add full-ack, haml-mode, and sass-mode

--- hoov.el
+++ hoov.el
@@ -1,5 +1,7 @@
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
@@ -10,14 +12,21 @@
 (color-theme-hoov)
 
 ;; Pymacs
-(require 'pymacs)
-(add-to-list 'pymacs-load-path (concat dotfiles-dir "pymacs"))
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(eval-after-load "pymacs"
+  '(add-to-list 'pymacs-load-path (concat dotfiles-dir "pymacs")))
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
 (setq ac-ropemacs-loaded t)
 
-(add-hook 'python-mode-hook '(lambda ()
-                               (local-set-key (kbd "RET") 'newline-and-indent)))
+(add-hook 'python-mode-hook
+    (lambda ()
+        (local-set-key (kbd "RET") 'newline-and-indent)
+    ))
 
 ;; yasnippet
 
@@ -25,7 +34,7 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory (concat dotfiles-dir "/vendor/yasnippet/snippets"))
-
+;
 ;; Auto Complete
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/auto-complete"))
 (require 'auto-complete-config)
@@ -36,11 +45,12 @@
   (ac-ropemacs-setup)
   (setq ac-sources (append '(ac-source-yasnippet) ac-sources)))
 (add-hook 'python-mode-hook 'ac-python-mode-setup)
-
+;
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/textmate.el"))
 (require 'textmate)
-(textmate-mode)
 (require 'peepopen)
+(setq ns-pop-up-frames nil)
+(textmate-mode)
 
 ;; Mercurial support
 (require 'mercurial)
@@ -50,6 +60,9 @@
 
 ;; Column number
 (column-number-mode)
+
+;; Idle highlight (wow, what a helpful comment)
+(idle-highlight)
 
 ;; Line numbers
 (global-linum-mode 1)
@@ -61,3 +74,29 @@
                      (let ((w (length (number-to-string
                                        (count-lines (point-min) (point-max))))))
                        (concat "%" (number-to-string w) "d ")) line) 'face 'linum)))
+
+;; Set up flymake to use pyflakes
+(when (load "flymake" t) 
+      (defun flymake-pyflakes-init () 
+        (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                           'flymake-create-temp-inplace)) 
+           (local-file (file-relative-name 
+                        temp-file 
+                        (file-name-directory buffer-file-name)))) 
+          (list "pyflakes" (list local-file)))) 
+
+      (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pyflakes-init))) 
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+;; haml + sass
+(require 'haml-mode)
+(require 'sass-mode)
+
+
+(setq ack-root-directory-functions '(textmate-project-root))
+(autoload 'ack-same "full-ack" nil t)
+(autoload 'ack "full-ack" nil t)
+(autoload 'ack-find-same-file "full-ack" nil t)
+(autoload 'ack-find-file "full-ack" nil t)
